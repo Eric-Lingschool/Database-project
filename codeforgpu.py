6cc03af61213e9e3c2b7966fc47c17822f2a6b84
Eric-Lingschool <22646@burnside.school.nz>
2024-06-08 22:16:56 +1200

    first commit
    milestone 1

---
+++ codeforgpu.py
@@ -0,0 +1,14 @@
+'''code for the gpu database'''
+import sqlite3
+from colorama import Fore, Style, init
+
+#Define user name and password saparate by user and admin
+users = {
+"Eric":{'password':'123456','role':'admin'},
+}
+
+#Initialize colorma
+init()
+
+def listeverything():
+    print("hi")
