02ee43c737a4c53e96270fb49f130f70d1080a93
Eric-Lingschool <22646@burnside.school.nz>
2024-06-12 20:21:16 +1200

    $ git commit

--- codeforgpu.py
+++ codeforgpu.py
@@ -14,6 +14,20 @@ with open('username and password.txt', 'r') as file:
     for line in file:
         line_count += 1
 
+#code to connect to database
+def connectdatabase(sql, heading, spc1, spc2, spc3, spc4): #sql is what command in the sql interface you want to use             The spc# varables are to determine the spacing between the data
+    db = sqlite3.connect("GPU.db") #connect to database
+    cursor = db.cursor()
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(heading)
+    for gpus in results:
+        print(Fore.WHITE + f"{gpus[1]:<{spc1}}{gpus[2]:<{spc2}}{gpus[3]:<{spc3}}{gpus[4]:<{spc4}}")
+    print("")
+    db.close()
+    
+
+
 #login code
 def login():
     loginstate = False
@@ -23,10 +37,10 @@ def login():
         passtrials = 0  #how many tries they took to enter their password
         while True:
             print(Fore.WHITE + "Type 'exit' to exit the login interface")
-            username = input(Fore.WHITE + "Please enter your username: ")
+            username = input(Fore.WHITE + "Please enter your username: \n")
             if username == 'exit':
                 break
-            password = input("Please enter your password: ")
+            password = input("Please enter your password: \n")
             sections = userpass.split(", ")
             for i in range(line_count):
                 if sections[i * 4 + 1] == username and sections[i * 4 + 2] == password:
@@ -51,29 +65,33 @@ def new_user(): #creating new user code
     user_id = line_count
     admin = False
     adminpass = "adminpower"
-    new_username = input("Please enter your desired username:")
+    new_username = input("Please enter your desired username: \n")
     if new_username == adminpass:
-        new_username = input(Fore.GREEN + "You are now an admin of the database, please choose a new username!")
+        new_username = input(Fore.GREEN + "You are now an admin of the database, please choose a new username! \n")
         admin = True
-    new_password = input(Fore.LIGHTBLUE_EX + "Please enter your desired password:")
+    new_password = input(Fore.LIGHTBLUE_EX + "Please enter your desired password: \n")
     with open('username and password.txt', 'w') as file:
         userpass = file.write(f"{user_id}, {new_username}, {new_password}, {admin}, ")
 
 
 
-def getall():
-    '''print all data in gpu table'''
-    db = sqlite3.connect()
-    cursor = db.cursor()
-   
-    #Lakers_Player
-    sql= "SELECT * FROM Lakers_Player_2023_24;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
+def getallfromdatabase(): #GET EVERYTHING FROM THE DATABASE!
+    connectdatabase("SELECT * FROM GPU;", Fore.GREEN + "Name                Manufacturer  Clock speed id  Memory id", 20, 14, 16, 14)
+    connectdatabase("SELECT * FROM Memory;", Fore.GREEN + "Memory size  Memory bus", 13, 10, 0, 0)
+    connectdatabase("SELECT * FROM clock_speed;", Fore.GREEN + "Base clock  Memory clock", 13, 10, 0, 0)
+    print("It's a bit messy but YOU asked for everything at once.")
 
 
 
-login()
+
+
+
+getallfromdatabase()
+
+
+
+
+
 
 
 
